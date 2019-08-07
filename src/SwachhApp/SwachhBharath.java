a097a70eeca546867583e18c72b12443d5983032
Manasa Sama <53785057+mansasama@users.noreply.github.com>
2019-08-07 09:24:10 +0530

    Update SwachhBharath.java

--- src/SwachhApp/SwachhBharath.java
+++ src/SwachhApp/SwachhBharath.java
@@ -1,13 +1,8 @@
 package SwachhApp;
 
-
 public class SwachhBharath {
-
 	public static void main(String[] args) {
-	
 		@SuppressWarnings("unused")
 		registerORLogin user1 = new registerORLogin();
-
 	}
-
 }
